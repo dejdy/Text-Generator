7f18873e783efb75451aa3c7cb5609b9f3716a9e
Jan Dejdar <dejdy@ifeelgood.cz>
2018-10-15 19:22:11 +0200

    Adapt to Trump's tweets.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import os
 import click
+import json
 from markov_chain import *
 
 
@@ -13,6 +14,23 @@ def load_text(path):
         data = data.replace(i, ' ')
     return data.split()
 
+def load_json(path):
+    to_add_space = ".!?:;,"
+    with open(path) as json_file:
+        text = json_file.read()
+    for i in to_add_space:
+        text = text.replace(i, i+' ')
+
+    json_data = json.loads(text)
+
+    ret = []
+    for i in json_data:
+        words = ""
+        if '@' not in i['text'] and 'http' not in i['text']:
+            words += i['text']
+        ret.append(words.split())
+
+    return ret
 
 def generate_words(chain, order, no_sentences=1):
     words = ""
@@ -21,17 +39,31 @@ def generate_words(chain, order, no_sentences=1):
         words += cur[i] + ' '
 
     while words.count('.') < no_sentences:
-        next_word = chain[cur].pick_at_random()
+        try:
+            next_word = chain[cur].pick_at_random()
+        except:
+            cur = pick_start(chain)
+            for i in range(0, order-1):
+                words += cur[i] + ' '
+            next_word = cur[-1]
+
         words += next_word + ' '
         cur = cur[1:] + (next_word,)
         
     return words
 
 
-def build_chain(directory, order):
+def build_chain(directory, order, use_json):
     chain = create_chain()
     for filename in os.listdir(directory):
-        add_to_chain(chain, load_text(os.path.join(directory, filename)), order)
+        if not use_json:
+            if filename.endswith(".txt"):
+                add_to_chain(chain, load_text(os.path.join(directory, filename)), order)
+        else:
+            if filename.endswith(".json"):
+                tweets = load_json(os.path.join(directory, filename))
+                for tweet in tweets:
+                    add_to_chain(chain, tweet, order)
     return chain
 
 
@@ -45,9 +77,12 @@ def build_chain(directory, order):
 @click.option('-n', '--no-sentences',
                 default=1, show_default=True,
                 help='Number of sentences to generate')
-def main(directory, order, no_sentences):
+@click.option('-j', '--use-json',
+                default=False, show_default=True,
+                help='Search for .json files instead of .txt')
+def main(directory, order, no_sentences, use_json):
     """Random text generator based on nth-order Markov chain"""
-    chain = build_chain(directory, order)
+    chain = build_chain(directory, order, use_json=use_json)
     words = generate_words(chain, order, no_sentences=no_sentences)
     print(words)
 
